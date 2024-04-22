d0927d28347a2941fcdb84e630ba4d7311ffcfe0
Никита Мирошников <mintus.play@gmail.com>
2024-04-22 13:29:46 +0300

    first save

---
+++ content/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Tamga-transit</title>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/custom.css">
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="d-flex flex-row-reverse align-items-end">
+            <div class="m-1">
+                <select class="form-select form-select-sm" aria-label="Small select example">
+                    <option selected value="rus">Русский</option>
+                    <option value="eng">Английский</option>
+                </select>
+            </div>
+            <div class="m-1 pb-1">Language</div>
+        </div>
+    </div>
+    <header class="sticky-top">
+        <nav class="navbar navbar-expand-lg c_header_back">
+            <div class="container">
+                <a class="navbar-brand" href="#">TRANSIT</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-1 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Главная</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Услуги</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Тарифы</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Таможенные режимы</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex">
+                        <button class="btn btn-outline-primary ms-1">Регистрация</button>
+                        <button class="btn btn-outline-primary ms-1">Вход</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+require_once 'content/header.php';
+?>
+
+<main>
+    <div class="container c_banner_img">
+        <p class="z-1 ps-6 pt-5 fs-2 c_font_otl">
+            Транспортировка и <br> отслеживание <br> таможенных грузов
+        </p>
+        <div>
+            <div class="row align-items-center text-center mx-5 mt-5">
+                <div class="col b-box mx-5 my-3">
+                    <img src="/assets/res/bb1.png" alt="..." style="height: 2rem;" class="my-2">
+                    <p>Экспор\Импорт</p>
+                </div>
+                <div class="col b-box mx-5 my-3">
+                    <img src="/assets/res/bb2.png" alt="..." style="height: 2rem;" class="my-2">
+                    <p>Отслеживание</p>
+                </div>
+                <div class="col b-box mx-5 my-3">
+                    <img src="/assets/res/bb3.png" alt="..." style="height: 2rem;" class="my-2">
+                    <p>Транзит</p>
+                </div>
+                <div class="col b-box mx-5 my-3">
+                    <img src="/assets/res/bb4.png" alt="..." style="height: 2rem;" class="my-2">
+                    <p>Аренда пломбы</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr class="container delimeter">
+
+    <div class="container">
+        <p class="fs-5 ms-5">Наши преимущества</p>
+
+        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="5000">
+                    <div class="row mx-3">
+                        <div class="col clearfix b-box p-2 mx-2">
+                            <img src="..." class="col-md-6 float-md-end mb-3 ms-md-3" alt="...">
+                            <p>
+                                A paragraph of placeholder text. We're using it here to show the use of the clearfix class. We're adding quite a few meaningless phrases here to demonstrate how the columns interact here with the floated image.
+                            </p>
+                        </div>
+                        <div class="col clearfix b-box p-2 mx-2">
+                            <img src="..." class="col-md-6 float-md-end mb-3 ms-md-3" alt="...">
+                            <p>
+                                A paragraph of placeholder text. We're using it here to show the use of the clearfix class. We're adding quite a few meaningless phrases here to demonstrate how the columns interact here with the floated image.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item" data-bs-interval="5000">
+                    <div class="row mx-3">
+                        <div class="col clearfix b-box p-2 mx-2">
+                            <img src="..." class="col-md-6 float-md-end mb-3 ms-md-3" alt="...">
+                            <p>
+                                A paragraph of placeholder text. We're using it here to show the use of the clearfix class. We're adding quite a few meaningless phrases here to demonstrate how the columns interact here with the floated image.
+                            </p>
+                        </div>
+                        <div class="col clearfix b-box p-2 mx-2">
+                            <img src="..." class="col-md-6 float-md-end mb-3 ms-md-3" alt="...">
+                            <p>
+                                A paragraph of placeholder text. We're using it here to show the use of the clearfix class. We're adding quite a few meaningless phrases here to demonstrate how the columns interact here with the floated image.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Prev</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
+</main>
+
+<?php
+require_once 'content/header.php';
+?>
